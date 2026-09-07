d522f6d8410d9b1331a0927b6437534de2915803
Jack Walker <>
2026-09-06 23:05:22 -0600

    Hello world!
    The initial ideas behind this library, along with the first really basic types:
    - Point3D
    - Vector3D

---
+++ src/.net/AdaptiveNetwork.Geometry.Core/Qualities/IEquatableWithinTolerance.cs
@@ -0,0 +1,17 @@
+﻿namespace AdaptiveNetwork.Geometry.Core.Qualities
+{
+    /// <summary>
+    /// Defines a method for determining whether two objects are almost equal, allowing for a specified tolerance in the comparison.
+    /// </summary>
+    /// <typeparam name="TIn">The type of the objects to compare.</typeparam>
+    public interface IEquatableWithinTolerance<in TIn>
+    {
+        /// <summary>
+        /// Determines whether the current object is almost equal to another object of the same type, within a specified tolerance.
+        /// </summary>
+        /// <param name="other">The other object to compare with the current object.</param>
+        /// <param name="tolerance">The tolerance within which the two objects are considered almost equal.</param>
+        /// <returns>True if the current object is almost equal to the other object within the specified tolerance; otherwise, false.</returns>
+        bool EquatableWithinTolerance(TIn other, double tolerance = 0);
+    }
+}
---
+++ src/.net/AdaptiveNetwork.Geometry.Core/Types/Vector3D.cs
@@ -0,0 +1,81 @@
+﻿using AdaptiveNetwork.Geometry.Core.Qualities;
+
+using System.Diagnostics.CodeAnalysis;
+
+namespace AdaptiveNetwork.Geometry.Core.Types
+{
+    /// <summary>
+    /// Represents a three-dimensional vector with double precision components.
+    /// </summary>
+    public struct Vector3D : IEquatableWithinTolerance<Vector3D>, IEquatableWithinTolerance<Point3D>
+    {
+        /// <summary>
+        /// Gets the X component of the vector.
+        /// </summary>
+        public double X { get; private set; }
+
+        /// <summary>
+        /// Gets the Y component of the vector.
+        /// </summary>
+        public double Y { get; private set; }
+
+        /// <summary>
+        /// Gets the Z component of the vector.
+        /// </summary>
+        public double Z { get; private set; }
+
+        public Vector3D() { }
+
+        /// <summary>
+        /// Initializes a new instance of the Vector3D struct with the specified X, Y, and Z components.
+        /// </summary>
+        /// <param name="x">The X component of the vector.</param>
+        /// <param name="y">The Y component of the vector.</param>
+        /// <param name="z">The Z component of the vector.</param>
+        public Vector3D(double x, double y, double z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        /// <summary>
+        /// Returns a new Vector3D instance representing the origin (0, 0, 0).
+        /// </summary>
+        /// <returns>A Vector3D instance with all components set to 0.</returns>
+        public static Vector3D Origin() => new(0, 0, 0);
+
+        /// <summary>
+        /// Returns a string representation of the Vector3D instance in the format "X, Y, Z".
+        /// </summary>
+        /// <returns>A string representation of the Vector3D instance.</returns>
+        public override readonly string ToString()
+        {
+            return $"{X}, {Y}, {Z}";
+        }
+
+        public override bool Equals([NotNullWhen(true)] object? obj)
+        {
+            throw new NotImplementedException("The Vector3D values should be compared using the EquatableWithinTolerance methods with a specified tolerance");
+        }
+
+        public override int GetHashCode()
+        {
+            throw new NotImplementedException("The Vector3D values should be compared using the EquatableWithinTolerance methods with a specified tolerance");
+        }
+
+        public bool EquatableWithinTolerance(Vector3D other, double tolerance = 0)
+        {
+            return Math.Abs(X - other.X) <= tolerance &&
+                   Math.Abs(Y - other.Y) <= tolerance &&
+                   Math.Abs(Z - other.Z) <= tolerance;
+        }
+
+        public bool EquatableWithinTolerance(Point3D other, double tolerance = 0)
+        {
+            return Math.Abs(X - other.X) <= tolerance &&
+                   Math.Abs(Y - other.Y) <= tolerance &&
+                   Math.Abs(Z - other.Z) <= tolerance;
+        }
+    }
+}
